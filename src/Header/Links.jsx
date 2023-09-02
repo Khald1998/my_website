0907dade6fd8a5338c168a8f417717ce7bc7f347
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-09-02 20:02:51 +0300

    -

--- src/Header/Links.jsx
+++ src/Header/Links.jsx
@@ -2,7 +2,9 @@ import Link from "./Link";
 function Links(props) {
   return (
     <>
-      <div className={` ${props.ScreenMode=='big'?"flex items-center px-5 text-lg":"flex flex-col justify-start items-start text-start"}`}>
+      <div className={` ${props.ScreenMode=='big'?"flex items-center md:px-0 px-5 text-lg":"flex flex-col justify-start items-start text-start"}
+      xl:px-0
+      `}>
         <Link text="Home" link="#Home" />
         <Link text="About me" link="#About"/>
         <Link text="Skills" link="#Skills"/>
--- src/Header/main.jsx
+++ src/Header/main.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import Link from "./Links";
 import Brand from "./Brand";
-import Closed from "../assets/BM-closed.svg";
-import Opned from "../assets/BM-opned.svg";
 import { Divide as Hamburger } from "hamburger-react";
 
 function App() {
@@ -11,7 +9,7 @@ function App() {
 
   useEffect(() => {
     document.addEventListener("scroll", () => {
-      const scrollTop = window.pageYOffset;
+      const scrollTop = window.scrollY;
       if (scrollTop === 0) {
         setIsTop(true);
       } else {
@@ -20,32 +18,35 @@ function App() {
     });
   }, []);
 
-  // const toggleMenu = () => {
-  //   setIsOpen(!isOpen);
-  // };
-
   return (
     <>
-      <section
-        className={`${isOpen?'h-[200px]':'h-[65px]'} w-full px-6 py-3 fixed text-whitey transition-all duration-500 z-50 ${
-          isTop ? "" : "bg-nav backdrop-blur"
-        } `}
-      >
-        <nav className="flex items-center justify-between">
-          <Brand />
-          <div className="max-md:hidden">
-            <Link ScreenMode="big" />
-          </div>
-          <div className="md:hidden">
-            <Hamburger toggled={isOpen} toggle={setOpen} />
-          </div>
-        </nav>
+      <section className={`  md:flex md:justify-center ${isTop ? "" : "md:mt-5"} transition-all duration-200`} >
         <div
-          className={`absolute transition-all duration-500 w-full md:hidden ${
-            isOpen ? "top-[60px]" : "top-[-300px]"
-          }`}
+          className={`
+          fixed text-whitey transition-all duration-500 z-50 w-full
+          ${isOpen ? "h-[200px]" : "h-[65px]"} ${isTop ? "" : "bg-nav backdrop-blur md:rounded-3xl"}
+              
+          md:flex md:items-center md:w-10/12   
+          `}
         >
-          <Link ScreenMode="small" />
+          <nav className="max-md:py-2 max-md:px-6 md:w-full md:px-6 xl:px-16">
+            <div className="flex items-center justify-between ">
+              <Brand />
+              <div className="max-md:hidden">
+                <Link ScreenMode="big" />
+              </div>
+              <div className="md:hidden">
+                <Hamburger toggled={isOpen} toggle={setOpen} />
+              </div>
+            </div>
+            <div
+              className={`absolute transition-all duration-500 w-full md:hidden ${
+                isOpen ? "top-[60px]" : "top-[-300px]"
+              }`}
+            >
+              <Link ScreenMode="small" />
+            </div>
+          </nav>
         </div>
       </section>
     </>
